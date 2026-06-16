06593a03bd8647e0ca87fcf2a36755ee199b2352
SvPr1993 <mrtventana@gmail.com>
2026-06-16 19:19:02 +0300

    first commit

--- _settings/settings.py
+++ _settings/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 from pathlib import Path
 from decouple import config
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = '_settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
